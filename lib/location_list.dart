5e4d00f1fbac6fd7d95173f40471756a6afe6312
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-24 19:31:09 +0100

    Add Unit Tests

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,58 +1,49 @@
 import 'package:flutter/material.dart';
+import 'models/location.dart';
 import 'location_detail.dart';
 import 'styles.dart';
-import 'models/location.dart';
 
 class LocationList extends StatelessWidget {
-  final List<Location> _locations;
+  final List<Location> locations;
 
-  const LocationList(this._locations, {super.key});
+  const LocationList(this.locations, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Locations',
-          style: Styles.navBarTitle,
-        ),
-      ),
+      appBar: AppBar(title: Text("Locations", style: Styles.navBarTitle)),
       body: ListView.builder(
-        itemCount: _locations.length,
+        itemCount: locations.length,
         itemBuilder: _listViewItemBuilder,
       ),
     );
   }
 
   Widget _listViewItemBuilder(BuildContext context, int index) {
-    var location = _locations[index];
+    var location = locations[index];
     return ListTile(
-      contentPadding: const EdgeInsets.all(10),
-      leading: _itemThumbnail(location),
-      title: _itemTitle(location),
-      onTap: () => _navigationToLocationDetail(context, _locations[index]),
-    );
+        contentPadding: const EdgeInsets.all(10.0),
+        leading: _itemThumbnail(location),
+        title: _itemTitle(location),
+        onTap: () => _navigateToLocationDetail(context, index));
   }
 
-  void _navigationToLocationDetail(BuildContext context, Location location) {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => LocationDetail(location)));
+  void _navigateToLocationDetail(BuildContext context, int locationID) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LocationDetail(locationID),
+        ));
   }
 
   Widget _itemThumbnail(Location location) {
     return Container(
       constraints: const BoxConstraints.tightFor(width: 100.0),
-      child: Image.network(
-        location.url,
-        fit: BoxFit.fitHeight,
-      ),
+      child: Image.network(location.url, fit: BoxFit.fitWidth),
     );
   }
 
   Widget _itemTitle(Location location) {
-    return Text(
-      location.name,
-      style: Styles.textDefault,
-    );
+    return Text(location.name, style: Styles.textDefault);
   }
 }
